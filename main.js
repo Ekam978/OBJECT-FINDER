64f443c34ba4b568b35b80bdd69bfd7a2d93e6fe
Ekam978 <73385604+Ekam978@users.noreply.github.com>
2021-03-22 03:07:24 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function setup() {
     canvas = createCanvas(400,325);
@@ -18,6 +19,34 @@ function modelLoaded() {
     status = true;
 }
 
-function draw() {
+function gotResult(results , error) {
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    results = objects;
+}
+
+function draw() 
+{
     image(video,0,0,400,325);
+
+    if (status != "") 
+    {
+        objectDetector.detect(video,gotResult);
+
+        for (i = 0; i < objects.length; i++) {
+            if (objects[i].label == object_search) {
+                var synth = window.speechSynthesis;
+                speak_data = objects[i].label + " Found";
+                var utterThis = new SpeechSynthesisUtterance(speak_data);
+                synth.speak(utterThis);
+                document.getElementById("status_of_model").innerHTML = "Model Has Detected Object";
+                document.getElementById("status").innerHTML = "Status : " + objects[i].label + " Found";
+                percentage = floor(objects[i].confidence * 100);
+                text(objects[i].label + " " + percentage + "%" , objects[i].x + 15 , objects[i].y + 15);
+                rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+            }
+        }
+    }
 }
